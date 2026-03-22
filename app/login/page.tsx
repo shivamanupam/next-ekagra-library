f30a4d946eb9e418f41ab26f8246aab5072422bc
Shivam Anupam <shivamanupam@Shivams-MacBook-Air.local>
2026-03-23 01:33:42 +0530

    Basic admin login page

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div>
+        <h1>Login</h1>
+        <p>This will be the login page</p>
+      </div>
+    </>
+  );
+};
+
+export default Login;
