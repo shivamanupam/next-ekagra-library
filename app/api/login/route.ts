ea69e4c9e922ee1695a02d832d9eacdd93d08148
Shivam Anupam <shivamanupam@Shivams-MacBook-Air.local>
2026-03-23 02:34:53 +0530

    Add JWT-based admin session

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,6 +2,8 @@ import { connectDB } from "@/lib/db";
 import { Admin } from "@/models/Admin";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 export async function POST(req: Request) {
   try {
@@ -33,9 +35,21 @@ export async function POST(req: Request) {
       );
     }
 
+    const token = await jwt.sign(
+      { adminId: admin._id, email: admin.email },
+      process.env.JWT_SECRET,
+      { expiresId: "1d" }
+    );
+
+    (await cookies()).set("admin_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+    });
+
     return NextResponse.json({
       message: "Login successful",
-      admin: { email: admin.email },
     });
   } catch (error) {
     return NextResponse.json(
